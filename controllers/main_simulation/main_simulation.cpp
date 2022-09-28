5783ee1777d36d6f7f4860b5402d35dd8b464259
Julien Bourque <julien.bourque@polymtl.ca>
2022-09-28 11:08:32 -0400

    Formated code

--- controllers/main_simulation/main_simulation.cpp
+++ controllers/main_simulation/main_simulation.cpp
@@ -10,141 +10,142 @@
 /****************************************/
 /****************************************/
 
-CMainSimulation::CMainSimulation() :
-   m_pcDistance(NULL),
-   m_pcPropellers(NULL),
-   m_pcRNG(NULL),
-   m_pcRABA(NULL),
-   m_pcRABS(NULL),
-   m_pcPos(NULL),
-   m_pcBattery(NULL),
-   m_uiCurrentStep(0) {}
+CMainSimulation::CMainSimulation()
+    : m_pcDistance(NULL),
+      m_pcPropellers(NULL),
+      m_pcRNG(NULL),
+      m_pcRABA(NULL),
+      m_pcRABS(NULL),
+      m_pcPos(NULL),
+      m_pcBattery(NULL),
+      m_uiCurrentStep(0) {}
 
 /****************************************/
 /****************************************/
 
 void CMainSimulation::Init(TConfigurationNode& t_node) {
-   try {
-      /*
-       * Initialize sensors/actuators
-       */
-      m_pcDistance   = GetSensor  <CCI_CrazyflieDistanceScannerSensor>("crazyflie_distance_scanner");
-      m_pcPropellers = GetActuator  <CCI_QuadRotorPositionActuator>("quadrotor_position");
-      /* Get pointers to devices */
-      m_pcRABA   = GetActuator<CCI_RangeAndBearingActuator>("range_and_bearing");
-      m_pcRABS   = GetSensor  <CCI_RangeAndBearingSensor  >("range_and_bearing");
-      try {
-         m_pcPos = GetSensor  <CCI_PositioningSensor>("positioning");
-      }
-      catch(CARGoSException& ex) {}
-      try {
-         m_pcBattery = GetSensor<CCI_BatterySensor>("battery");
-      }
-      catch(CARGoSException& ex) {}      
-   }
-   catch(CARGoSException& ex) {
-      THROW_ARGOSEXCEPTION_NESTED("Error initializing the crazyflie sensing controller for robot \"" << GetId() << "\"", ex);
-   }
-   /*
-    * Initialize other stuff
-    */
-   /* Create a random number generator. We use the 'argos' category so
-      that creation, reset, seeding and cleanup are managed by ARGoS. */
-   m_pcRNG = CRandom::CreateRNG("argos");
+    try {
+        /*
+         * Initialize sensors/actuators
+         */
+        m_pcDistance = GetSensor<CCI_CrazyflieDistanceScannerSensor>(
+            "crazyflie_distance_scanner");
+        m_pcPropellers =
+            GetActuator<CCI_QuadRotorPositionActuator>("quadrotor_position");
+        /* Get pointers to devices */
+        m_pcRABA =
+            GetActuator<CCI_RangeAndBearingActuator>("range_and_bearing");
+        m_pcRABS = GetSensor<CCI_RangeAndBearingSensor>("range_and_bearing");
+        try {
+            m_pcPos = GetSensor<CCI_PositioningSensor>("positioning");
+        } catch (CARGoSException& ex) {
+        }
+        try {
+            m_pcBattery = GetSensor<CCI_BatterySensor>("battery");
+        } catch (CARGoSException& ex) {
+        }
+    } catch (CARGoSException& ex) {
+        THROW_ARGOSEXCEPTION_NESTED(
+            "Error initializing the crazyflie sensing controller for robot \""
+                << GetId() << "\"",
+            ex);
+    }
+    /*
+     * Initialize other stuff
+     */
+    /* Create a random number generator. We use the 'argos' category so
+       that creation, reset, seeding and cleanup are managed by ARGoS. */
+    m_pcRNG = CRandom::CreateRNG("argos");
 
-   m_uiCurrentStep = 0;
-   Reset();
+    m_uiCurrentStep = 0;
+    Reset();
 }
 
 /****************************************/
 /****************************************/
 
 void CMainSimulation::ControlStep() {
-   // Dummy behavior: takeoff for 10 steps, 
-   // then moves in a square shape for 200 steps then lands.
+    // Dummy behavior: takeoff for 10 steps,
+    // then moves in a square shape for 200 steps then lands.
 
-   int nInitSteps = 10;
-   int nTotalSteps = 400;
-   // Takeoff
-   if ( m_uiCurrentStep < nInitSteps ) {
-      TakeOff();
-      m_cInitialPosition = m_pcPos->GetReading().Position;
-   } 
-   else if ((m_uiCurrentStep - nInitSteps) < nTotalSteps) {
-      // Square pattern
-      CVector3 trans(0.0f, 0.0f, 0.0f);
-      if ( (m_uiCurrentStep - nInitSteps) < nTotalSteps/4 ) {
-         trans.SetX(1.0f);
-      }
-      else if ( (m_uiCurrentStep - nInitSteps) < 2*nTotalSteps/4 ) {
-         trans.SetY(1.0f);
-      }
-      else if ( (m_uiCurrentStep - nInitSteps) < 3*nTotalSteps/4 ) {
-         trans.SetX(-1.0f);
-      }
-      else {
-         trans.SetY(-1.0f);
-      }
-      CVector3 currentPosition = m_pcPos->GetReading().Position;
-      CVector3 relativePositionCommand = (m_cInitialPosition + trans) - currentPosition; 
-      
-      m_pcPropellers->SetRelativePosition(relativePositionCommand);
-   }
-   else {
-      Land();
-   }   
-   // Print current position.
-   LOG << "Position (x,y,z) = (" << m_pcPos->GetReading().Position.GetX() << ","
-       << m_pcPos->GetReading().Position.GetY() << ","
-       << m_pcPos->GetReading().Position.GetZ() << ")" << std::endl; 
+    int nInitSteps = 10;
+    int nTotalSteps = 400;
+    // Takeoff
+    if (m_uiCurrentStep < nInitSteps) {
+        TakeOff();
+        m_cInitialPosition = m_pcPos->GetReading().Position;
+    } else if ((m_uiCurrentStep - nInitSteps) < nTotalSteps) {
+        // Square pattern
+        CVector3 trans(0.0f, 0.0f, 0.0f);
+        if ((m_uiCurrentStep - nInitSteps) < nTotalSteps / 4) {
+            trans.SetX(1.0f);
+        } else if ((m_uiCurrentStep - nInitSteps) < 2 * nTotalSteps / 4) {
+            trans.SetY(1.0f);
+        } else if ((m_uiCurrentStep - nInitSteps) < 3 * nTotalSteps / 4) {
+            trans.SetX(-1.0f);
+        } else {
+            trans.SetY(-1.0f);
+        }
+        CVector3 currentPosition = m_pcPos->GetReading().Position;
+        CVector3 relativePositionCommand =
+            (m_cInitialPosition + trans) - currentPosition;
 
-   // Print current battery level
-   const CCI_BatterySensor::SReading& sBatRead = m_pcBattery->GetReading();
-   LOG << "Battery level: " << sBatRead.AvailableCharge  << std::endl;
+        m_pcPropellers->SetRelativePosition(relativePositionCommand);
+    } else {
+        Land();
+    }
+    // Print current position.
+    LOG << "Position (x,y,z) = (" << m_pcPos->GetReading().Position.GetX()
+        << "," << m_pcPos->GetReading().Position.GetY() << ","
+        << m_pcPos->GetReading().Position.GetZ() << ")" << std::endl;
 
-   // Look here for documentation on the distance sensor: /root/argos3/src/plugins/robots/crazyflie/control_interface/ci_crazyflie_distance_scanner_sensor.h
-   // Read and print distance sensor measurements
-   CCI_CrazyflieDistanceScannerSensor::TReadingsMap sDistRead = 
-      m_pcDistance->GetReadingsMap();
-   auto iterDistRead = sDistRead.begin();
-   if (sDistRead.size() == 4) {
-      LOG << "Front dist: " << (iterDistRead++)->second  << std::endl;
-      LOG << "Left dist: "  << (iterDistRead++)->second  << std::endl;
-      LOG << "Back dist: "  << (iterDistRead++)->second  << std::endl;
-      LOG << "Right dist: " << (iterDistRead)->second  << std::endl;
-   }
+    // Print current battery level
+    const CCI_BatterySensor::SReading& sBatRead = m_pcBattery->GetReading();
+    LOG << "Battery level: " << sBatRead.AvailableCharge << std::endl;
 
-   // Increase step counter
-   m_uiCurrentStep++;
+    // Look here for documentation on the distance sensor:
+    // /root/argos3/src/plugins/robots/crazyflie/control_interface/ci_crazyflie_distance_scanner_sensor.h
+    // Read and print distance sensor measurements
+    CCI_CrazyflieDistanceScannerSensor::TReadingsMap sDistRead =
+        m_pcDistance->GetReadingsMap();
+    auto iterDistRead = sDistRead.begin();
+    if (sDistRead.size() == 4) {
+        LOG << "Front dist: " << (iterDistRead++)->second << std::endl;
+        LOG << "Left dist: " << (iterDistRead++)->second << std::endl;
+        LOG << "Back dist: " << (iterDistRead++)->second << std::endl;
+        LOG << "Right dist: " << (iterDistRead)->second << std::endl;
+    }
+
+    // Increase step counter
+    m_uiCurrentStep++;
 }
 
 /****************************************/
 /****************************************/
 
 bool CMainSimulation::TakeOff() {
-   CVector3 cPos = m_pcPos->GetReading().Position;
-   if(Abs(cPos.GetZ() - 2.0f) < 0.01f) return false;
-   cPos.SetZ(2.0f);
-   m_pcPropellers->SetAbsolutePosition(cPos);
-   return true;
+    CVector3 cPos = m_pcPos->GetReading().Position;
+    if (Abs(cPos.GetZ() - 2.0f) < 0.01f) return false;
+    cPos.SetZ(2.0f);
+    m_pcPropellers->SetAbsolutePosition(cPos);
+    return true;
 }
 
 /****************************************/
 /****************************************/
 
 bool CMainSimulation::Land() {
-   CVector3 cPos = m_pcPos->GetReading().Position;
-   if(Abs(cPos.GetZ()) < 0.01f) return false;
-   cPos.SetZ(0.0f);
-   m_pcPropellers->SetAbsolutePosition(cPos);
-   return true;
+    CVector3 cPos = m_pcPos->GetReading().Position;
+    if (Abs(cPos.GetZ()) < 0.01f) return false;
+    cPos.SetZ(0.0f);
+    m_pcPropellers->SetAbsolutePosition(cPos);
+    return true;
 }
 
 /****************************************/
 /****************************************/
 
-void CMainSimulation::Reset() {
-}
+void CMainSimulation::Reset() {}
 
 /****************************************/
 /****************************************/
