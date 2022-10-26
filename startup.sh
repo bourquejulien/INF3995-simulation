d2a2711970537b915bffd17e70e263d67ab4c0fd
Julien Bourque <julien.bourque@polymtl.ca>
2022-10-25 22:39:23 -0400

    Added seed randomisation on startup

--- startup.sh
+++ startup.sh
@@ -3,6 +3,8 @@
 set -m
 trap 'kill $(jobs -p); printf "\nExiting\n"; exit' SIGINT SIGTERM
 
+bash randomize_seed.sh
+
 python3 -m http.server --directory /root/client $WEBVIZ_PORT &
 argos3 -c experiments/$1 &
 
@@ -10,4 +12,3 @@ while true
 do
   sleep 1
 done
-
